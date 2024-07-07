296017543ac789eba4ebd7092bd438a9471245ef
Basar Buyukkahraman <basarbk@yahoo.com>
2024-07-07 23:42:44 +0300

    0202-submit

--- src/pages/sign-up/index.jsx
+++ src/pages/sign-up/index.jsx
@@ -1,10 +1,29 @@
+import { useState } from "react";
+
 export function SignUp() {
+  const [email, setEmail] = useState();
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    fetch("/api/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email }),
+    });
+  };
+
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <h1>Sign Up</h1>
       <label htmlFor="email">E-mail</label>
-      <input id="email" autoComplete="off" />
-      <button disabled>Sign Up</button>
+      <input
+        id="email"
+        autoComplete="off"
+        onChange={(event) => setEmail(event.target.value)}
+      />
+      <button disabled={!email}>Sign Up</button>
     </form>
   );
 }
